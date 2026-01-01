08637fd0ff756b7f4978e9d2d27c0641e0ec7bf2
kayaniyasir <kianiyasir@gmail.com>
2026-01-01 10:29:15 +0300

    Just priting all datatypes

---
+++ PrintDataType1.java
@@ -0,0 +1,57 @@
+ackage printdatatype1;
+public class PrintDataType1 {
+    public static void main(String[] args) {
+        System.out.println("---Homework no 1 \t\t Abdullah Alkharashi\t\t ID 222211573");
+        System.out.print("DataType                \t");    
+        System.out.print("MinValue                \t"); 
+        System.out.print("MaxValue                \t"); 
+        System.out.print("Size in Bits                \t"); 
+        System.out.println("Size in Bytes                \t"); 
+        
+        System.out.print(Short.TYPE+"                        \t");
+        System.out.print(Short.MIN_VALUE+"                   \t");
+        System.out.print(Short.MAX_VALUE+"                   \t\t");
+        System.out.print(Short.SIZE+"                      \t");
+        System.out.println(Short.BYTES+"\t\t");
+        
+        System.out.print(Integer.TYPE+"                        \t");
+        System.out.print(Integer.MIN_VALUE+"                  \t");
+        System.out.print(Integer.MAX_VALUE+"              \t\t");
+        System.out.print(Integer.SIZE+"                        \t");
+        System.out.println(Integer.BYTES+"                    \t\t");
+        
+        System.out.print(Long.TYPE+"                        \t");
+        System.out.print(Long.MIN_VALUE+"          \t");
+        System.out.print(Long.MAX_VALUE+"       \t\t");
+        System.out.print(Long.SIZE+"                            \t");
+        System.out.println(Long.BYTES+"                  \t\t");
+        
+        System.out.print(Float.TYPE+"                       \t");
+        System.out.print(Float.MIN_VALUE+"            \t\t");
+        System.out.print(Float.MAX_VALUE+"                  \t\t");
+        System.out.print(Float.SIZE+"                        \t");
+        System.out.println(Float.BYTES+"\t\t");
+        
+        System.out.print(Double.TYPE+"                        \t");
+        System.out.print(Double.MIN_VALUE+"                \t");
+        System.out.print(Double.MAX_VALUE+"        \t\t");
+        System.out.print(Double.SIZE+"                        \t");
+        System.out.println(Double.BYTES+"                  \t\t");
+        
+        System.out.print(Character.TYPE+"                        \t");
+        System.out.print((int)Character.MIN_VALUE+"                            \t");
+        System.out.print((int)Character.MAX_VALUE+"                    \t\t");
+        System.out.print(Character.SIZE+"                          \t");
+        System.out.println(Character.BYTES+"                                \t\t");
+        
+        System.out.print(Byte.TYPE+"                        \t");
+        System.out.print(Byte.MIN_VALUE+"                        \t");
+        System.out.print(Byte.MAX_VALUE+"                        \t\t");
+        System.out.print(Byte.SIZE+"                          \t");
+        System.out.println(Byte.BYTES+"                                \t\t");
+        
+     
+        
+    }
+    
+}
